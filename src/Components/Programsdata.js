8cf50b079c951b8aaf86bd6296849e822d7ba89b
mahiiram <mahendran.ramar9797@gmail.com>
2023-06-25 17:54:08 +0530

    new commit

--- src/Components/Programsdata.js
+++ src/Components/Programsdata.js
@@ -23,15 +23,15 @@ function Programsdata() {
     return (
         <div className='programsData'>
             <div onClick={weightLoss} className='card'>
-                    <img src='https://media.istockphoto.com/id/610540064/vector/man-before-and-after-sports-cartoon-vector-illustration.jpg?s=612x612&w=0&k=20&c=XA67h0btw3hjVISeO-iyITYo8GDVmfDRIfEj0gwuWAc=' alt=''/>
+                    <img className='cardimg' src='https://media.istockphoto.com/id/610540064/vector/man-before-and-after-sports-cartoon-vector-illustration.jpg?s=612x612&w=0&k=20&c=XA67h0btw3hjVISeO-iyITYo8GDVmfDRIfEj0gwuWAc=' alt=''/>
                     <h4 style={{color:'black',textAlign:'center'}}>Weight loss</h4>
             </div>
             <div onClick={weightgain} className='card'>
-                    <img src='https://w7.pngwing.com/pngs/117/794/png-transparent-weight-training-sports-training-bodyweight-exercise-weight-gain-others-miscellaneous-physical-fitness-sport-thumbnail.png' alt=''/>
+                    <img className='cardimg' src='https://w7.pngwing.com/pngs/117/794/png-transparent-weight-training-sports-training-bodyweight-exercise-weight-gain-others-miscellaneous-physical-fitness-sport-thumbnail.png' alt=''/>
                     <h4 style={{color:'black',textAlign:'center'}}>Weight Gain</h4>
             </div>
             <div onClick={gymtraining} className='card'>
-                    <img src='https://e7.pngegg.com/pngimages/576/568/png-clipart-fitness-centre-physical-fitness-logo-work-out-hand-sticker-thumbnail.png' alt=''/>
+                    <img className='cardimg' src='https://e7.pngegg.com/pngimages/576/568/png-clipart-fitness-centre-physical-fitness-logo-work-out-hand-sticker-thumbnail.png' alt=''/>
                     <h4 style={{color:'black',textAlign:'center'}}>Gym training</h4>
             </div>
             {/* <div onClick={yogatraining} className='card'>
